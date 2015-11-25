dea527ea78a87a9ed5103895481f4f08c793654c
Vignesh Rajagopalan <me@aarvay.com>
2015-11-25 21:52:43 +0530

    Add some tests
    Tests are boon. Regret adding it so late. This small test suite
    revealed a wierd problem with NIFs. I get varied output for the same
    input when run multiple times.

--- test/graphql_parser_test.exs
+++ test/graphql_parser_test.exs
@@ -1,8 +1,53 @@
-defmodule GraphqlParserTest do
+defmodule GraphQL.ParserTest do
   use ExUnit.Case
-  doctest GraphqlParser
+  # doctest GraphQL.Parser
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ExUnit.TestHelper
+
+  test "simple query" do
+    assert_parse_success "{ field }",
+    %{definitions: [%{directives: nil, kind: "OperationDefinition",
+      loc: %{end: 10, start: 1}, name: nil, operation: "query",
+      selectionSet: %{kind: "SelectionSet", loc: %{end: 10, start: 1},
+        selections: [%{alias: nil, arguments: nil, directives: nil,
+           kind: "Field", loc: %{end: 8, start: 3},
+           name: %{kind: "Name", loc: %{end: 8, start: 3}, value: "field"},
+           selectionSet: nil}]}, variableDefinitions: nil}], kind: "Document",
+    loc: %{end: 10, start: 1}}
+  end
+
+  test "complex query with variable inline values" do
+    assert_parse_success "{ field(complex: { a: { b: [ $var ] } }) }",
+    %{definitions: [%{directives: nil, kind: "OperationDefinition",
+      loc: %{end: 43, start: 1}, name: nil, operation: "query",
+      selectionSet: %{kind: "SelectionSet", loc: %{end: 43, start: 1},
+        selections: [%{alias: nil,
+           arguments: [%{kind: "Argument", loc: %{end: 40, start: 9},
+              name: %{kind: "Name", loc: %{end: 16, start: 9},
+                value: "complex"},
+              value: %{fields: [%{kind: "ObjectField",
+                   loc: %{end: 38, start: 20},
+                   name: %{kind: "Name", loc: %{end: 21, start: 20},
+                     value: "a"},
+                   value: %{fields: [%{kind: "ObjectField",
+                        loc: %{end: 36, start: 25},
+                        name: %{kind: "Name", loc: %{end: 26, start: 25},
+                          value: "b"},
+                        value: %{kind: "ArrayValue", loc: %{end: 36, start: 28},
+                          values: [%{kind: "Variable",
+                             loc: %{end: 34, start: 30},
+                             name: %{kind: "Name", loc: %{end: 34, start: 30},
+                               value: "var"}}]}}], kind: "ObjectValue",
+                     loc: %{end: 38, start: 23}}}], kind: "ObjectValue",
+                loc: %{end: 40, start: 18}}}], directives: nil, kind: "Field",
+           loc: %{end: 41, start: 3},
+           name: %{kind: "Name", loc: %{end: 8, start: 3}, value: "field"},
+           selectionSet: nil}]}, variableDefinitions: nil}], kind: "Document",
+    loc: %{end: 43, start: 1}}
+  end
+
+  test "failure when fragments named on" do
+    assert_parse_failure "fragment on on on { on }",
+    "1.10-11: syntax error, unexpected on"
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,13 @@
+defmodule ExUnit.TestHelper do
+  import ExUnit.Assertions
+
+  def assert_parse_success(input, output) do
+    assert {:ok, output} == GraphQL.Parser.parse(input)
+  end
+
+  def assert_parse_failure(input, output) do
+    assert {:error, output} == GraphQL.Parser.parse(input)
+  end
+end
+
 ExUnit.start()
