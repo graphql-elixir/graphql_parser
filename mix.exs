189c6740798246d0ec3ae2828a87b4b0e5718b1c
Vignesh Rajagopalan <me@aarvay.com>
2015-12-14 02:07:58 +0530

    Prepare for hex publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule GraphQL.Parser.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :graphql_parser,
-     version: "0.0.1",
+     name: "GraphQL.Parser",
+     version: @version,
      elixir: "~> 1.1",
      compilers: [:libgraphqlparser, :nif] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
+     docs: docs,
      deps: deps]
   end
 
@@ -16,7 +22,36 @@ defmodule GraphQL.Parser.Mixfile do
   end
 
   defp deps do
-    [{:poison, "~> 1.5"}]
+    [{:poison, "~> 1.5"},
+     {:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp description do
+    """
+    An elixir interface for libgraphqlparser implemented as a NIF for parsing
+    GraphQL.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Vignesh Rajagopalan"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github/aarvay/graphql_parser"},
+     files: ["lib", "src", "Makefile", "mix.exs", "README.md",
+             "libgraphqlparser/ast/ast.ast", "libgraphqlparser/ast/*.py",
+             "libgraphqlparser/c/*.cpp", "libgraphqlparser/c/*.h",
+             "libgraphqlparser/*.h", "libgraphqlparser/*.cpp",
+             "libgraphqlparser/*.lpp", "libgraphqlparser/*.hh",
+             "libgraphqlparser/*.hpp", "libgraphqlparser/*.ypp",
+             "libgraphqlparser/CMakeLists.txt", "libgraphqlparser/LICENSE",
+             "libgraphqlparser/PATENTS", "libgraphqlparser/README.md"]]
+  end
+
+  defp docs do
+    [source_ref: "v#{@version}",
+     extras: ["README.md"],
+     main: "README"]
   end
 end
 
